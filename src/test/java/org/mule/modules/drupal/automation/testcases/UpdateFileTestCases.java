a216c3883685a1c3863462fa3e097fdcba6e953a
jsaliba <justin.saliba@ricston.coma>
2013-12-09 09:50:08 +0100

    Added UpdateFile test cases

---
+++ src/test/java/org/mule/modules/drupal/automation/testcases/UpdateFileTestCases.java
@@ -0,0 +1,55 @@
+package org.mule.modules.drupal.automation.testcases;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.modules.drupal.model.File;
+import org.mule.modules.tests.ConnectorTestUtils;
+
+public class UpdateFileTestCases extends DrupalTestParent {
+	
+	@Before
+	public void setUp() throws Exception {
+		initializeTestRunMessage("updateFileTestData");
+
+		String fileName = getTestRunMessageValue("fileName");
+		String filePath = getTestRunMessageValue("filePath");
+		File file = generateFile(fileName, filePath);
+		
+		file = createFile(file);
+		upsertOnTestRunMessage("file", file);
+		upsertOnTestRunMessage("fileId", file.getFid());
+	}
+	
+	@Category({RegressionTests.class})
+	@Test
+	public void testUpdateFile() {
+		try {
+			File createdFile = getTestRunMessageValue("file");
+			String newFileName = getTestRunMessageValue("newFileName");
+			Integer fileId = getTestRunMessageValue("fileId");
+			
+			createdFile.setFilename(newFileName);
+
+			upsertOnTestRunMessage("fileRef", createdFile);
+			
+			File updatedFile = runFlowAndGetPayload("update-file");
+			assertEquals(updatedFile, fileId);
+			assertEquals(updatedFile.getFilename(), newFileName);
+		}
+		catch (Exception e) {
+			fail(ConnectorTestUtils.getStackTrace(e));
+		}
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		Integer fileId = getTestRunMessageValue("fileId");
+		deleteFile(fileId);
+	}
+
+}
