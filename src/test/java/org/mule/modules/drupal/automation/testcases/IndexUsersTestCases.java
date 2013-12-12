b85f8f3a4edcd10326a620269b66f5c43ec72ead
jsaliba <justin.saliba@ricston.coma>
2013-12-12 09:50:16 +0100

    Added IndexUsers test cases

---
+++ src/test/java/org/mule/modules/drupal/automation/testcases/IndexUsersTestCases.java
@@ -0,0 +1,74 @@
+package org.mule.modules.drupal.automation.testcases;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections.Predicate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.modules.drupal.model.User;
+import org.mule.modules.tests.ConnectorTestUtils;
+import org.mule.util.CollectionUtils;
+
+public class IndexUsersTestCases extends DrupalTestParent {
+
+	@Before
+	public void setUp() throws Exception {
+		initializeTestRunMessage("indexUsersTestData");
+		
+		List<User> users = getTestRunMessageValue("users");
+		List<Integer> userIds = new ArrayList<Integer>();
+		for (User user : users) {
+			User createdUser = createUser(user);
+			userIds.add(createdUser.getUid());
+		}
+		upsertOnTestRunMessage("userIds", userIds);
+	}
+	
+	@Category({RegressionTests.class})
+	@Test
+	public void testIndexUsers() {
+		try {
+			List<Integer> userIds = getTestRunMessageValue("userIds");
+			List<User> retrievedUsers = runFlowAndGetPayload("index-users");
+			
+			/*
+			 * Retrieved users is always going to be equal to or larger than the amount of users
+			 * that were created in the @Before method. There has to be at least a user already
+			 * present with which you have to login with.
+			 */
+			assertTrue(retrievedUsers.size() >= userIds.size());
+
+			/*
+			 * Assert that the created users in the @Before method are retrieved by the API request.
+			 */
+			for (Integer userId : userIds) {
+				boolean found = false;
+				for (User user : retrievedUsers) {
+					if (user.getUid() == userId) {
+						found = true;
+						break;
+					}
+				}
+				assertTrue(found);
+			}
+		}
+		catch (Exception e) {
+			fail(ConnectorTestUtils.getStackTrace(e));
+		}
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		List<Integer> userIds = getTestRunMessageValue("userIds");
+		for (Integer userId : userIds) {
+			deleteUser(userId);
+		}
+	}
+	
+}
